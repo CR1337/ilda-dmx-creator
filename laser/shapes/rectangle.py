807d25284d1dccfa14aae77ece1b94c2a58a4bb4
Christian Raue <captain_2@gmx.de>
2024-12-11 17:06:41 +0100

    center width and height

--- laser/shapes/rectangle.py
+++ laser/shapes/rectangle.py
@@ -13,7 +13,21 @@ class Rectangle(Polygon):
         color_gradient: ColorGradient,
         point_density: float | None = None
     ):
+        top_left = np.array(top_left)
+        bottom_right = np.array(bottom_right)
         top_right = np.array([bottom_right[0], top_left[1]])
         bottom_left = np.array([top_left[0], bottom_right[1]])
         points = [top_left, bottom_left, bottom_right, top_right]
         super().__init__(points, color_gradient, point_density)
+
+    @property
+    def center(self) -> np.ndarray:
+        return (self._points[0] + self._points[2]) / 2
+
+    @property
+    def width(self) -> float:
+        return self._points[2][0] - self._points[0][0]
+    
+    @property
+    def height(self) -> float:
+        return self._points[3][1] - self._points[1][1]
