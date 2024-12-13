98cd2fe2854a0c63777f5fd18c82cf532a05bc2d
Christian Raue <captain_2@gmx.de>
2024-12-13 17:43:48 +0100

    worked on 2025 show

--- 2025/2025.py
+++ 2025/2025.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from factory import Factory, IldxFrame, DmxFrame
-from noise import Noise1D, Noise3D
+from noise import Noise1D, Noise2D, Noise3D
 from laser.color import Color, ColorGradient
 import laser.shapes as ls
 
@@ -17,6 +17,78 @@ import numpy as np
 from typing import List, Tuple
 from itertools import cycle, product
 
+
+class WormholeDisplacement:
+
+    _warp_noise: Noise3D
+
+    def __init__(self, rel_t: float):
+        self._warp_noise = Noise3D.toroidal(
+            amplitude=(rel_t / 15) * 0.04
+        )
+
+    def __call__(self, shape: ls.Shape, p: np.ndarray, s: float, t: float) -> np.ndarray:
+        return p + self._warp_noise([p[0], p[1], t])
+
+
+class LaserDotDisplacement:
+
+    _position: List[float]
+    _time: float
+    _exclusion_zones: List[ls.Shape]
+
+    _noise_x: Noise2D
+    _noise_y: Noise2D
+
+    def __init__(self, position: List[float], time: float, exclusion_zones: List[ls.Shape]):
+        self._position = position
+        self._time = time
+        self._exclusion_zones = exclusion_zones
+
+        self._noise_x = Noise3D(amplitude = 0.02, frequency=[0.3], seed=42 + position[0])
+        self._noise_y = Noise3D(amplitude = 0.02, frequency=[0.3], seed=1337 + position[0])
+
+    def __call__(self, shape: ls.Shape, p: np.ndarray, s: float, t: float) -> np.ndarray:
+        new_p = (
+            p + 
+            np.array([self._noise_x([p[0], p[1], t]), self._noise_y([p[1], p[0], t])]) 
+            * (self._time - t)
+        )
+        # TODO
+        # for zone in self._exclusion_zones:
+        #     q = zone.nearest_point(new_p, t)
+        #     direction = q - new_p
+        #     d = np.linalg.norm(direction)
+        #     new_direction = d ** 2 * direction / d
+        #     new_p = new_p + new_direction
+        return new_p
+    
+
+class ShieldLaserDotDisplacement:
+
+    _segment: ls.Line
+    _noise: Noise1D
+
+    def __init__(self, segment: ls.Line):
+        self._segment = segment
+        self._noise = Noise1D(amplitude=0.06, frequency=[1], seed=42 + segment.start[0])
+
+    def __call__(self, shape: ls.Shape, p: np.ndarray, s: float, t: float) -> np.ndarray:
+        p += self._segment.tangent(s) * self._noise.get_value(t)
+        return p
+        
+
+class ShieldSegmentDisplacement:
+
+    _noise: Noise2D
+
+    def __init__(self):
+        self._noise = Noise2D(amplitude=0.01, frequency=[10, 10])
+
+    def __call__(self, shape: ls.Shape, p: np.ndarray, s: float, t: float) -> np.ndarray:
+        n = shape.normal(s, t)
+        return p + n * self._noise([s, t])
+    
 
 class ShowFactory:
 
@@ -148,6 +220,7 @@ class ShowFactory:
     # endregion
 
     E: float = 2.1 * (1 / FPS)
+    viewer_ellipse: ls.Ellipse
 
     def __init__(self):
         self._load_fixtures()
@@ -181,8 +254,17 @@ class ShowFactory:
         for exclusion_zone in self.exclusion_zones:
             self.factory._ildx_factory.add_exclusion_zone(exclusion_zone)
 
+        self.viewer_ellipse: ls.Ellipse = ls.Ellipse(
+            self.viewer_box.center, 
+            [
+                1.15 * self.viewer_box.width / np.sqrt(2),
+                1.15 * self.viewer_box.height / np.sqrt(2)
+            ],
+            ColorGradient(Color.black())
+        )
+
     def _eqal_time(self, t1: float, t2: float) -> bool:
-        return t1 - self.E <= t2 <= t1 + self.E
+        return self._time_between(t2, t1 - self.E, t1 + self.E)
     
     def _time_between(self, t: float, t1: float, t2: float) -> bool:
         return t1 <= t <= t2
@@ -368,18 +450,6 @@ class ShowFactory:
             dmx_frame += self.fog_fury_l['fog']['off']()
             dmx_frame += self.fog_fury_r['fog']['off']()
 
-    class WormholeDisplacement:
-
-        _warp_noise: Noise3D
-
-        def __init__(self, rel_t: float):
-            self._warp_noise = Noise3D.toroidal(
-                amplitude=(rel_t / 15) * 0.04
-            )
-
-        def __call__(self, shape: ls.Shape, p: np.ndarray, s: float, t: float) -> np.ndarray:
-            return p + self._warp_noise([p[0], p[1], t])
-
     def wormhole_factory_function(self, ildx_frame: IldxFrame, dmx_frame: DmxFrame):
         """
         6:15 - 07:30
@@ -430,14 +500,8 @@ class ShowFactory:
                     s -= 1
             color_gradient.add_color(s, color)
         
-        wormhole = ls.Ellipse(
-            self.viewer_box.center, 
-            [
-                1.15 * self.viewer_box.width / np.sqrt(2),
-                1.15 * self.viewer_box.height / np.sqrt(2)
-            ],
-            color_gradient
-        )
+        wormhole = self.viewer_ellipse.copy()
+        wormhole.set_color_gradient(color_gradient)
 
         if ildx_frame.rel_t >= wobble_start:
             wobble_noise = Noise1D(
@@ -449,7 +513,7 @@ class ShowFactory:
             ])
 
         if ildx_frame.rel_t >= displacement_start:
-            wormhole.displace(self.WormholeDisplacement(ildx_frame.rel_t - displacement_start))
+            wormhole.displace(WormholeDisplacement(ildx_frame.rel_t - displacement_start))
 
         ildx_frame += wormhole
 
@@ -510,23 +574,12 @@ class ShowFactory:
         """
         pass
 
-    class LaserDotDisplacement:
-
-        _position: List[float]
-        _time: float
-
-        def __init__(self, position: List[float], time: float):
-            self._position = position
-            self._time = time
-
-        def __call__(self, shape: ls.Shape, p: np.ndarray, s: float, t: float) -> np.ndarray:
-            ...  # TODO
-
     def attack_shields_factory_function(self, ildx_frame: IldxFrame, dmx_frame: DmxFrame):
         """
         08:50 - 10:20
         """
-        explosion_duration = 0.2
+        # Laser dots
+        explosion_duration = 0.1
         explosion_times = [
             self._string_to_seconds(t) for t in [
                 "9:30",
@@ -556,7 +609,7 @@ class ShowFactory:
             [-0.8872600354646378,  -0.17821141293174353]
         ]
         displacements = [
-            self.LaserDotDisplacement(position, time) 
+            LaserDotDisplacement(position, time, self.exclusion_zones) 
             for position, time in zip(explosion_positions, explosion_times)
         ]
         color_gradients = [
@@ -576,6 +629,104 @@ class ShowFactory:
                 continue
             ildx_frame += dot.displace(displacement)
 
+        # Shields
+        shield_start_t = 9 * 60 + 46
+        if ildx_frame.t < shield_start_t:
+            return
+        shield_duration = 35
+
+        n_segments = 10
+        dot_distance = 0.1
+
+        n_time_intervals = n_segments + 2
+        interval_duration = shield_duration / n_time_intervals
+
+        interval_index = (ildx_frame.t - shield_start_t) / interval_duration
+
+        dot_timings = [
+            t + Noise1D(amplitude=0.15).get_value(t)
+            for t in np.linspace(
+                shield_start_t + interval_duration, 
+                shield_start_t + (n_segments * interval_duration), 
+                n_segments + 1
+            )
+        ]
+
+        shield_ellipse = self.viewer_ellipse.copy()
+        segments = shield_ellipse.segments(n_segments)
+        random.Random(x=1337).shuffle(segments)
+        dot_positions = [
+            s.center - s.normal(0.5, ildx_frame.t) * dot_distance
+            for s in segments
+        ]
+        dots = [
+            ls.Point(p, ColorGradient(Color.green()))
+            for p in dot_positions
+        ]
+
+        interval_0_progress = (ildx_frame.t - shield_start_t) / interval_duration
+
+        blue = Color(0, Noise1D(amplitude=0.5, frequency=[0.01]).get_value(ildx_frame.t), 1)
+        black_gradient = ColorGradient(Color.black())
+        blue_gradient = ColorGradient(blue)
+        black_to_blue_gradient = ColorGradient(Color.black(), blue)
+        black_to_blue_gradient.add_color(interval_0_progress, blue)
+        black_to_green_gradient = ColorGradient(Color.black().interpolate_hsv(Color.green(), interval_0_progress))
+        green_gradient = ColorGradient(Color.green())
+        red_gradient = ColorGradient(Color.red())
+        white_gradient = ColorGradient(Color.white())
+
+        segment_displacements = [
+            ShieldSegmentDisplacement()
+            for _ in  segments
+        ]
+        dot_displacements = [
+            ShieldLaserDotDisplacement(segment)
+            for segment in segments
+        ]
+
+        for segment, dot, timing, segment_displacement, dot_displacement in zip(
+            segments, dots, dot_timings, segment_displacements, dot_displacements
+        ):
+            if interval_index == 0:
+                dot.set_color_gradient(black_to_green_gradient)
+                segment.set_color_gradient(black_to_blue_gradient)
+
+            elif interval_index > 0 and interval_index < n_time_intervals - 1:
+                if ildx_frame.t < timing:
+                    dot.set_color_gradient(green_gradient)
+                    segment.set_color_gradient(blue_gradient)
+                    dot.displace(dot_displacement)
+                else:
+                    if self._time_between(ildx_frame.t, timing - explosion_duration, timing + explosion_duration):
+                        dot.set_color_gradient(white_gradient)
+                    else:
+                        dot.set_color_gradient(black_gradient)
+                    segment.set_color_gradient(red_gradient)
+                    segment.displace(segment_displacement)
+
+            if interval_index == n_time_intervals - 1:
+                last_interval_progress = (ildx_frame.t - shield_start_t + (n_time_intervals - 1) + interval_duration) / interval_duration
+                sub_segments_per_segment = 7
+                sub_segments: List[ls.Line] = []
+                for segment in segments:
+                    for i in range(sub_segments_per_segment):
+                        sub_segment_start = segment.start + (segment.end - segment.start) * i / sub_segments_per_segment
+                        sub_segment_end = segment.start + (segment.end - segment.start) * (i + 1) / sub_segments_per_segment
+                        sub_segments.append(ls.Line(sub_segment_start, sub_segment_end, ColorGradient(Color.red().interpolate_hsv(Color.black(), last_interval_progress))))
+
+                for sub_segment in sub_segments:
+                    rotation_noise = Noise1D(amplitude=0.1)
+                    position_noise = Noise2D(amplitude=0.1)
+                    sub_segment.translate(-sub_segment.center)
+                    sub_segment.rotate(rotation_noise.get_value(ildx_frame.t))
+                    sub_segment.translate(sub_segment.center)
+                    sub_segment.translate(position_noise.get_value(ildx_frame.t))
+                    ildx_frame += sub_segment
+            else:
+                ildx_frame += segment
+                ildx_frame += dot
+
     def battle_factory_function(self, ildx_frame: IldxFrame, dmx_frame: DmxFrame):
         """
         10:20 - 11:20
@@ -584,10 +735,10 @@ class ShowFactory:
         colors = ['red', 'green']
 
         if dmx_frame.index != dmx_frame.total_frames - 1:
-            amplitude_noises = [Noise1D(0.1, 0.1, seed=random.ranint(0, 100)) for _ in range(len(product(lamps, colors)))]
-            frequency_noises = [Noise1D(0.1, 0.1, seed=random.ranint(0, 100)) for _ in range(len(product(lamps, colors)))]
+            amplitude_noises = [Noise1D(0.1, 0.1, seed=42 + i) for i, _ in enumerate(product(lamps, colors))]
+            frequency_noises = [Noise1D(0.1, 0.1, seed=1337 + i) for i, _ in enumerate(product(lamps, colors))]
             
-            for i, lamp, color in enumerate(product(lamps, colors)):
+            for i, (lamp, color) in enumerate(product(lamps, colors)):
                 dmx_frame += lamp[color]['default'].pulse(
                     t=dmx_frame.t,
                     amplitude=amplitude_noises[i].get_value(dmx_frame.rel_t),
